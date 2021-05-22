d7bfaecdc6c743567c26b003165835067f372679
Zubenna <emelunna@gmail.com>
2021-05-22 18:55:47 +0100

    Added Email Validation in Doctor and Patient Model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,20 @@
 class Patient < ApplicationRecord
+   before_save :downcase_email
+   
    has_many :appointments
    has_many :doctors, through: :appointments, dependent: :destroy
    validates_presence_of :firstname, :lastname, :phone_number, :email
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
    validates_uniqueness_of :email
+   validates :email, 
+             presence: true,
+             length: { maximum: 50 },
+             format: { with: VALID_EMAIL_REGEX }
+   validates :password_digest, presence: true
+   
+   private
+
+   def downcase_email
+    self.email = email.downcase
+  end
 end
