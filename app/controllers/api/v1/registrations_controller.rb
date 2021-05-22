2a2950c6dfc80fa0b838b38a9e851263e179b276
Zubenna <emelunna@gmail.com>
2021-05-22 19:31:11 +0100

    Corrected Identation Error in Files

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,31 +1,31 @@
 module Api
-    module V1
-      class RegistrationsController < ApplicationController
+  module V1
+    class RegistrationsController < ApplicationController
+      def index
+        patients = Patient.order('created_at ASC')
+        render json: { status: 'SUCCESS', message: 'Loaded Patient list', data: patients }, status: :ok
+      end
 
-          def index
-            patients = Patient.order('created_at ASC');
-            render json: {status: 'SUCCESS', message: 'Loaded Patient list', data:patients},status: :ok
-          end
-  
-          def show
-            patient = Patient.find(params[:id]);
-            render json: {status: 'SUCCESS', message: 'Loaded Patient', data:patient},status: :ok
-          end
-          
-          def create
-            patient = Patient.new(patient_params)
-            if patient.save
-              render json: {status: 'SUCCESS', message: 'Saved Patient', data:patient},status: :ok
-            else
-              render json: {status: 'ERROR', message: 'Patient not saved', data:patient.errors},status: :unprocessable_entity
-            end
-          end
+      def show
+        patient = Patient.find(params[:id])
+        render json: { status: 'SUCCESS', message: 'Loaded Patient', data: patient }, status: :ok
+      end
 
-          private
-          
-          def patient_params
-            params.permit(:firstname, :lastname, :phone_number, :email, :address, :password_digest)
-          end
+      def create
+        patient = Patient.new(patient_params)
+        if patient.save
+          render json: { status: 'SUCCESS', message: 'Saved Patient', data: patient }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Patient not saved', data: patient.errors },
+                 status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def patient_params
+        params.permit(:firstname, :lastname, :phone_number, :email, :address, :password_digest)
       end
     end
   end
+end
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,36 +1,36 @@
 module Api
-    module V1
-      class SessionsController < ApplicationController
-          def create
-            patient = Patient.find_by(email: params[:email])
-            if patient
-              session[:patient_id] = patient.id
-              render json: { status: :created, logged_in: true, patient: patient}, status: 201
-            else
-              render json: { status: 401 }, status: 401
-            end
-          end
-          
-          def logged_in
-            if @current_patient
-              render json: {
-                logged_in: true,
-                patient: @current_patient
-              }, status: 201
-            else
-              render json: {
-                logged_in: false
-              }, status: 401
-            end
-          end
-        
-          def logout
-            reset_session
-            render json: {
-              status: 200,
-              logged_out: true
-            }
-          end
+  module V1
+    class SessionsController < ApplicationController
+      def create
+        patient = Patient.find_by(email: params[:email])
+        if patient
+          session[:patient_id] = patient.id
+          render json: { status: :created, logged_in: true, patient: patient }, status: 201
+        else
+          render json: { status: 401 }, status: 401
+        end
+      end
+
+      def logged_in
+        if @current_patient
+          render json: {
+            logged_in: true,
+            patient: @current_patient
+          }, status: 201
+        else
+          render json: {
+            logged_in: false
+          }, status: 401
+        end
+      end
+
+      def logout
+        reset_session
+        render json: {
+          status: 200,
+          logged_out: true
+        }
       end
     end
   end
+end
