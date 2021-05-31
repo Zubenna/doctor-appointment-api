0fc607de22b9bcd42d60b45c2656e6c74ec955bb
Zubenna <emelunna@gmail.com>
2021-05-31 06:48:02 +0100

    Modified route for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,11 +18,7 @@ Rails.application.routes.draw do
     end
   end
   
-  namespace 'api' do
-    namespace 'v1' do 
-      post '/login',    to: 'sessions#create'
-      post '/logout',   to: 'sessions#destroy'
-      get '/logged_in', to: 'sessions#logged_in?'
-    end
-  end
+  get "api/v1/logged_in", to: "api/v1/sessions#logged_in?"
+  delete "api/v1/logout", to: "api/v1/sessions#destroy"
+  post "api/v1/login", to: "api/v1/sessions#create"
 end
