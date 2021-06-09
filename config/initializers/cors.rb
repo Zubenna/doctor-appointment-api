095bc340b4de8ac23c24a3528c6a9d9608141724
Zubenna <emelunna@gmail.com>
2021-06-09 04:47:54 +0100

    Added two url to cors origin parameter for testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins 'https://book-a-doctor.netlify.app'
+      origins 'https://book-a-doctor.netlify.app', 'http://localhost:3000'
      
       resource '*',
         headers: :any,
